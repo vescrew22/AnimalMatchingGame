367b937d405178670776a4ec4b4788ade29afd94
Andrii Veselovskyi <veskrew@gmail.com>
2025-02-04 20:41:00 +0200

    Bug Fix - It was able to click on empty buttons incrementing counts and finishing game earlier.

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -14,7 +14,8 @@
             AnimalButtons.IsVisible = true;
             PlayAgainButton.IsVisible = false;
 
-            List<string> animalEmoji = [
+            List<string> animalEmoji = 
+                [
                 "🐙", "🐙",
                 "🐡", "🐡",
                 "🐘", "🐘",
@@ -34,9 +35,40 @@
             }
         }
 
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text)
+                        && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
         }
     }
 
